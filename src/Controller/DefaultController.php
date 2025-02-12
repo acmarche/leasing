546263a041584f95792fd2149b8a69282b04c16c
jfsenechal <jfsenechal@gmail.com>
2025-02-12 11:05:39 +0100

    clean

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/leasing')]
 class DefaultController extends AbstractController
